c046382187d6f21bee83b9d1d42cf4bbfb1709e7
Sam Morrison <sorrison@gmail.com>
2012-10-29 00:43:36 +0000

    Permissions on swift.conf

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,9 +11,10 @@ class swift {
   }
 
   file { '/etc/swift/swift.conf':
-    ensure  => file,
+    ensure  => present,
     owner   => swift,
     group   => swift,
+    mode    => '0640',
     content => template("swift/${openstack_version}/swift.conf.erb"),
   }
 
