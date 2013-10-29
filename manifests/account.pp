7dc82ed9579e4eb53822c1477b2d3ba482803a45
Devendran Jagadisan <devendran.jagadisan@unimelb.edu.au>
2013-10-29 15:58:51 +1100

    modification for swift_1.10
    Change-Id: Ia360f8ec62612dd2fbcde9c7c0238c0bdfa8eb44

--- manifests/account.pp
+++ manifests/account.pp
@@ -1,12 +1,75 @@
 class swift::account($workers=2) {
 
   $openstack_version = hiera('openstack_version')
+  $dedicated_replication = hiera('swift::account::dedicated_replication')
+  $account_replication_port = hiera('swift::account::account_replication_port')
   $total_procs = 1 + $workers
 
   package { 'swift-account':
     ensure => present,
   }
 
+
+if $dedicated_replication == true {    
+   file { "/etc/swift/account-server":
+   ensure => "directory",
+   owner => "swift",
+   group => "swift",
+   mode => 750,    
+   }   
+   #request config     
+    file { '/etc/swift/account-server/request.conf':
+     ensure  => present,
+     owner   => swift,
+     group   => swift,
+     require => Package['swift-account'],
+     content => template("swift/${openstack_version}/account-server/request.conf.erb"),
+    }   
+   
+   #replication config     
+   file { '/etc/swift/account-server/replication.conf':
+    ensure  => present,
+    owner   => swift,
+    group   => swift,
+    require => Package['swift-account'],
+    content => template("swift/${openstack_version}/account-server/replication.conf.erb"),
+  }
+
+     exec {'start_account_server':
+        command => '/usr/bin/swift-init account-server start',
+        subscribe => [ File['/etc/swift/account-server/request.conf'],
+                       File['/etc/swift/swift.conf']],
+        refreshonly => true,
+        returns => [0,1]
+      }   
+     exec {'start_account_replicator':
+        command => '/usr/bin/swift-init account-replicator start',
+        subscribe => [ File['/etc/swift/account-server/replication.conf'],
+                       File['/etc/swift/swift.conf']],
+        refreshonly => true,
+        returns => [0,1]
+     }   
+     exec {'start_account_reaper':
+        command => '/usr/bin/swift-init account-reaper start',
+        subscribe => [ File['/etc/swift/account-server/request.conf'],
+                       File['/etc/swift/swift.conf']],
+        refreshonly => true,
+        returns => [0,1]
+     }   
+     exec {'start_account_auditor':
+        command => '/usr/bin/swift-init account-auditor start',
+        subscribe => [ File['/etc/swift/account-server/request.conf'],
+                       File['/etc/swift/swift.conf']],
+        refreshonly => true,
+        returns => [0,1]
+      }   
+    file { '/etc/swift/account-server.conf':
+    ensure  => absent,
+    }
+
+  }
+
+else {
   file { '/etc/swift/account-server.conf':
     ensure  => present,
     owner   => swift,
@@ -36,6 +99,7 @@ class swift::account($workers=2) {
                    File['/etc/swift/swift.conf']],
   }
 
+ }
   nagios::service {
     'http_swift-account_6002':
       check_command => 'check_swift_internal!6002';
--- manifests/object.pp
+++ manifests/object.pp
@@ -1,11 +1,73 @@
 class swift::object($workers=2) {
 
   $openstack_version = hiera('openstack_version')
+  $dedicated_replication = hiera('swift::object::dedicated_replication')
+  $object_replication_port = hiera('swift::object::object_replication_port')
   $total_procs = 1 + $workers
 
   package { 'swift-object':
     ensure => present,
   }
+
+if $dedicated_replication == true {    
+   file { "/etc/swift/object-server":
+   ensure => "directory",
+   owner => "swift",
+   group => "swift",
+   mode => 750,    
+   }   
+   #request config     
+    file { '/etc/swift/object-server/request.conf':
+     ensure  => present,
+     owner   => swift,
+     group   => swift,
+     require => Package['swift-object'],
+     content => template("swift/${openstack_version}/object-server/request.conf.erb"),
+    }
+   
+   #replication config     
+   file { '/etc/swift/object-server/replication.conf':
+    ensure  => present,
+    owner   => swift,
+    group   => swift,
+    require => Package['swift-object'],
+    content => template("swift/${openstack_version}/object-server/replication.conf.erb"),
+  }
+ 
+     exec {'start_object_server':
+        command => '/usr/bin/swift-init object-server start',
+        subscribe => [ File['/etc/swift/object-server/request.conf'],
+                       File['/etc/swift/swift.conf']],
+        refreshonly => true,
+        returns => [0,1]
+      }
+     exec {'start_object_replicator':
+        command => '/usr/bin/swift-init object-replicator start',
+        subscribe => [ File['/etc/swift/object-server/replication.conf'],
+                       File['/etc/swift/swift.conf']],
+        refreshonly => true,
+        returns => [0,1]
+     }  
+     exec {'start_object_reaper':
+        command => '/usr/bin/swift-init object-reaper start',
+        subscribe => [ File['/etc/swift/object-server/request.conf'],
+                       File['/etc/swift/swift.conf']],
+        refreshonly => true,
+        returns => [0,1]
+     }
+     exec {'start_object_auditor':
+        command => '/usr/bin/swift-init object-auditor start',
+        subscribe => [ File['/etc/swift/object-server/request.conf'],
+                       File['/etc/swift/swift.conf']],
+        refreshonly => true,
+        returns => [0,1]
+      }
+    file { '/etc/swift/object-server.conf':
+    ensure  => absent,
+    }
+  }
+
+else {
 
   file { '/etc/swift/object-server.conf':
     ensure  => present,
@@ -42,6 +104,7 @@ class swift::object($workers=2) {
     subscribe => [ File['/etc/swift/object-server.conf'],
                    File['/etc/swift/swift.conf']],
   }
+ }
 
   nagios::service {
     'http_swift-object_6000':
