43a5d7939f3b74704d8cbc6e9217c7d6eec60f84
Simon KP <simon.kelada@unimelb.edu.au>
2014-05-13 15:38:06 +1000

    Add logrotate rule for rsyncd
    Change-Id: I2df5912641e4e576a0ae5c809ef136fe705012f7

--- manifests/node.pp
+++ manifests/node.pp
@@ -2,12 +2,17 @@ class swift::node($rsync_connections=2) inherits swift {
 
   file { '/etc/rsyncd.conf':
     ensure  => present,
-    content => template("swift/${openstack_version}/rsync.conf.erb"),
+    content => template("swift/${::openstack_version}/rsync.conf.erb"),
   }
 
   file { '/etc/default/rsync':
     ensure => present,
     source => 'puppet:///modules/swift/rsync',
+  }
+
+  logrotate::rule { 'rsyncd':
+    path    => '/var/log/rsyncd.log',
+    options => [ 'rotate 52', 'weekly', 'delaycompress', 'compress', 'missingok', 'notifempty' ],
   }
 
   service { 'rsync':
@@ -21,7 +26,7 @@ class swift::node($rsync_connections=2) inherits swift {
     ensure => directory,
     owner  => swift,
     group  => swift,
- }
+  }
 
   package { 'xfsprogs':
     ensure => installed,
@@ -46,25 +51,25 @@ class swift::node($rsync_connections=2) inherits swift {
     require => File['/etc/swift/drive-audit.conf'],
   }
 
- cron { 'swift-recon':
-   ensure  => present,
-   command => '/usr/bin/swift-recon-cron /etc/swift/object-server.conf',
-   user    => 'swift',
-   minute  => 5,
-   require => File['/etc/swift/object-server.conf'],
- }
+  cron { 'swift-recon':
+    ensure  => present,
+    command => '/usr/bin/swift-recon-cron /etc/swift/object-server.conf',
+    user    => 'swift',
+    minute  => 5,
+    require => File['/etc/swift/object-server.conf'],
+  }
 
   file { '/etc/sysctl.d/60-swift.conf':
     owner  => root,
     group  => root,
-    mode   => 0644,
+    mode   => '0644',
     source => 'puppet:///modules/swift/60-swift-sysctl.conf',
     notify => Exec[sysctl-swift],
   }
 
   exec { 'sysctl-swift':
-    command     => '/sbin/sysctl -p /etc/sysctl.d/60-swift.conf',
-    unless      => '/usr/bin/test `sysctl -e -n net.nf_conntrack_max` -eq 262144',
+    command => '/sbin/sysctl -p /etc/sysctl.d/60-swift.conf',
+    unless  => '/usr/bin/test `sysctl -e -n net.nf_conntrack_max` -eq 262144',
   }
 
   $stg_hosts = hiera('firewall::swift_storage_hosts', [])
