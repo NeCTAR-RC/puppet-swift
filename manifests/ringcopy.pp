18f5572940f1870f9be5ee39d3dd8f62d446172a
Sina Sadeghi <sina@aptira.com>
2014-05-21 17:39:08 +1000

    swift cluster name support for ringcopy.pp
    Without this patch, only one set of ring files can be deployed
    per puppet instance.
    
    A new hiera variable is introduced, swift::cluster_name and ring
    files will serve from puppet:///swift/files/ring_file/cluster_name
    
    Change-Id: I9493ae7bf431b6bc4d9c9ae260d497d474695552

--- manifests/ringcopy.pp
+++ manifests/ringcopy.pp
@@ -1,10 +1,10 @@
-define swift::ringcopy()
-{
+define swift::ringcopy() inherits swift {
+
     file {"/etc/swift/${name}.ring.gz":
-          ensure => file,
-          owner => swift,
-          group => swift,
-          source => "puppet:///modules/swift/ring_file/${name}.ring.gz",
-          }
+      ensure => file,
+      owner  => swift,
+      group  => swift,
+      source => "puppet:///modules/swift/ring_file/${cluster_name}/${name}.ring.gz",
+    }
 
 }
