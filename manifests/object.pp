2c9d3bde69d38d1122dd32a1b18228dd82319be3
Sina Sadeghi <sina@aptira.com>
2014-05-21 17:10:07 +1000

    multi daemon config support for object.pp
    This patch allows the behaviour of the module to be controlled
    by a variable swift::multi_daemon_config. If false, the module
    behaves exactly as the previous version.
    
    If true, the module makes significant changes in
    /etc/swift/object* and /etc/init/swift-object*.conf, with a
    new configuration style and init scripts.
    
    If true, the user must also set the swift::ipaddress_regnet
    and swift::ipaddress_repnet and swift::object::object_rep_port
    variables in hiera.
    
    Change-Id: Iabe7b71bdb1fbcb4c375eb41f1c017ca73d3655d

--- manifests/object.pp
+++ manifests/object.pp
@@ -4,45 +4,159 @@ class swift::object($workers=2, $rsync_timeout=3600,
 {
 
   $total_procs = 1 + $workers
+  $multi_daemon_config = hiera('swift::multi_daemon_config')
 
   package { 'swift-object':
     ensure => present,
   }
 
-  file { '/etc/swift/object-server.conf':
-    ensure  => present,
-    owner   => swift,
-    group   => swift,
-    require => Package['swift-object'],
-    content => template("swift/${openstack_version}/object-server.conf.erb"),
-  }
+  if $multi_daemon_config == false {
 
-  service { 'swift-object':
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['/etc/swift/object-server.conf'],
-                   File['/etc/swift/swift.conf']],
-  }
+    file { '/etc/swift/object-server.conf':
+      ensure  => absent,
+      require => Package['swift-object'],
+    }
 
-  service { 'swift-object-replicator':
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['/etc/swift/object-server.conf'],
-                   File['/etc/swift/swift.conf']],
-  }
+    file { '/etc/swift/object-server':
+      ensure => directory,
+      owner  => swift,
+      group  => swift,
+      require => Package['swift-object'],
+    }
 
-  service { 'swift-object-updater':
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['/etc/swift/object-server.conf'],
-                   File['/etc/swift/swift.conf']],
-  }
+    file { '/etc/swift/object-server.conf':
+      ensure  => present,
+      owner   => swift,
+      group   => swift,
+      require => [ Package['swift-object'],
+                   File['/etc/swift/object-server']],
+      content => template("swift/${openstack_version}/object-server.conf.erb"),
+    }
 
-  service { 'swift-object-auditor':
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['/etc/swift/object-server.conf'],
-                   File['/etc/swift/swift.conf']],
+    service { 'swift-object':
+      ensure    => running,
+      enable    => true,
+      subscribe => [ File['/etc/swift/object-server.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-object-replicator':
+      ensure    => running,
+      enable    => true,
+      subscribe => [ File['/etc/swift/object-server.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-object-updater':
+      ensure    => running,
+      enable    => true,
+      subscribe => [ File['/etc/swift/object-server.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-object-auditor':
+      ensure    => running,
+      enable    => true,
+      subscribe => [ File['/etc/swift/object-server.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+  } else {
+
+    $ipaddress_regnet = hiera('swift::ipaddress_regnet')
+    $ipaddress_repnet = hiera('swift::ipaddress_repnet')
+    $object_rep_port = hiera('swift::object::object_rep_port')
+
+    file { '/etc/swift/object-server.conf':
+      ensure  => absent,
+      require => Package['swift-object'],
+    }
+
+    file { '/etc/swift/object-server':
+      ensure => directory,
+      owner  => swift,
+      group  => swift,
+      require => Package['swift-object'],
+    }
+
+    file { '/etc/swift/object-server/1.conf':
+      ensure  => present,
+      owner   => swift,
+      group   => swift,
+      require => [ Package['swift-object'],
+                   File['/etc/swift/object-server']],
+      content => template("swift/${openstack_version}/multi_daemon_config/object-server-reg.conf.erb"),
+    }
+
+    file { '/etc/swift/object-server/2.conf':
+      ensure  => present,
+      owner   => swift,
+      group   => swift,
+      require => [ Package['swift-object'],
+                   File['/etc/swift/object-server']],
+      content => template("swift/${openstack_version}/multi_daemon_config/object-server-rep.conf.erb"),
+    }
+
+    file { '/etc/init/swift-object.conf':
+      ensure  => present,
+      require => Package['swift-object'],
+      source  => 'puppet:///modules/swift/swift_init/swift-object.conf'
+    }
+
+    file { '/etc/init/swift-object-replicator.conf':
+      ensure  => present,
+      require => Package['swift-object'],
+      source  => 'puppet:///modules/swift/swift_init/swift-object-replicator.conf'
+    }
+
+    file { '/etc/init/swift-object-updater.conf':
+      ensure  => present,
+      require => Package['swift-object'],
+      source  => 'puppet:///modules/swift/swift_init/swift-object-updater.conf'
+    }
+
+    file { '/etc/init/swift-object-auditor.conf':
+      ensure  => present,
+      require => Package['swift-object'],
+      source  => 'puppet:///modules/swift/swift_init/swift-object-auditor.conf'
+    }
+
+    service { 'swift-object':
+      ensure    => running,
+      enable    => true,
+      require   => File['/etc/init/swift-object.conf'],
+      subscribe => [ File['/etc/swift/object-server/1.conf'],
+                     File['/etc/swift/object-server/2.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-object-replicator':
+      ensure    => running,
+      enable    => true,
+      require   => File['/etc/init/swift-object-replicator.conf'],
+      subscribe => [ File['/etc/swift/object-server/1.conf'],
+                     File['/etc/swift/object-server/2.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-object-updater':
+      ensure    => running,
+      enable    => true,
+      require   => File['/etc/init/swift-object-updater.conf'],
+      subscribe => [ File['/etc/swift/object-server/1.conf'],
+                     File['/etc/swift/object-server/2.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-object-auditor':
+      ensure    => running,
+      enable    => true,
+      require   => File['/etc/init/swift-object-auditor.conf'],
+      subscribe => [ File['/etc/swift/object-server/1.conf'],
+                     File['/etc/swift/object-server/2.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
   }
 
   if $converged_node == false {
