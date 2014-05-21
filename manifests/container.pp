f31008ade82560abc8a22e34769ef8089c9a64dc
Sina Sadeghi <sina@aptira.com>
2014-05-21 17:04:39 +1000

    multi daemon config support for container.pp
    This patch allows the behaviour of the module to be controlled
    by a variable swift::multi_daemon_config. If false, the module
    behaves exactly as the previous version.
    
    If true, the module makes significant changes in
    /etc/swift/container* and /etc/init/swift-container*.conf, with a
    new configuration style and init scripts.
    
    If true, the user must also set the swift::ipaddress_regnet
    and swift::ipaddress_repnet and
    swift::container::container_rep_port variables in hiera.
    
    Change-Id: Id22035a839ea403897c97498459db289b676bf8b

--- manifests/container.pp
+++ manifests/container.pp
@@ -2,45 +2,147 @@ class swift::container($workers=2) {
 
   $openstack_version = hiera('openstack_version')
   $total_procs = 1 + $workers
+  $multi_daemon_config = hiera('swift::multi_daemon_config')
 
   package { 'swift-container':
     ensure => present,
   }
 
-  file { '/etc/swift/container-server.conf':
-    ensure  => present,
-    owner   => swift,
-    group   => swift,
-    require => Package['swift-container'],
-    content => template("swift/${openstack_version}/container-server.conf.erb"),
-  }
+  if $multi_daemon_config == false {
 
-  service { 'swift-container':
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['/etc/swift/container-server.conf'],
-                   File['/etc/swift/swift.conf']],
-  }
+    file { '/etc/swift/container-server.conf':
+      ensure  => present,
+      owner   => swift,
+      group   => swift,
+      require => [ Package['swift-container'],
+                   File['/etc/swift/container-server']],
+      content => template("swift/${openstack_version}/container-server.conf.erb"),
+    }
 
-  service { 'swift-container-replicator':
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['/etc/swift/container-server.conf'],
-                   File['/etc/swift/swift.conf']],
-  }
+    service { 'swift-container':
+      ensure    => running,
+      enable    => true,
+      subscribe => [ File['/etc/swift/container-server.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
 
-  service { 'swift-container-updater':
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['/etc/swift/container-server.conf'],
-                   File['/etc/swift/swift.conf']],
-  }
+    service { 'swift-container-replicator':
+      ensure    => running,
+      enable    => true,
+      subscribe => [ File['/etc/swift/container-server.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
 
-  service { 'swift-container-auditor':
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['/etc/swift/container-server.conf'],
-                   File['/etc/swift/swift.conf']],
+    service { 'swift-container-updater':
+      ensure    => running,
+      enable    => true,
+      subscribe => [ File['/etc/swift/container-server.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-container-auditor':
+      ensure    => running,
+      enable    => true,
+      subscribe => [ File['/etc/swift/container-server.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+  } else {
+
+    $ipaddress_regnet = hiera('swift::ipaddress_regnet')
+    $ipaddress_repnet = hiera('swift::ipaddress_repnet')
+    $container_rep_port = hiera('swift::container::container_rep_port')
+
+    file { '/etc/swift/container-server.conf':
+      ensure  => absent,
+      require => Package['swift-container'],
+    }
+
+    file { '/etc/swift/container-server':
+      ensure => directory,
+      owner  => swift,
+      group  => swift,
+      require => Package['swift-container'],
+    }
+
+    file { '/etc/swift/container-server/1.conf':
+      ensure  => present,
+      owner   => swift,
+      group   => swift,
+      require => [ Package['swift-container'],
+                   File['/etc/swift/container-server']],
+      content => template("swift/${openstack_version}/multi_daemon_config/container-server-reg.conf.erb"),
+    }
+
+    file { '/etc/swift/container-server/2.conf':
+      ensure  => present,
+      owner   => swift,
+      group   => swift,
+      require => [ Package['swift-container'],
+                   File['/etc/swift/container-server']],
+      content => template("swift/${openstack_version}/multi_daemon_config/container-server-rep.conf.erb"),
+    }
+
+    file { '/etc/init/swift-container.conf':
+      ensure  => present,
+      require => Package['swift-container'],
+      source  => 'puppet:///modules/swift/swift_init/swift-container.conf'
+    }
+
+    file { '/etc/init/swift-container-replicator.conf':
+      ensure  => present,
+      require => Package['swift-container'],
+      source  => 'puppet:///modules/swift/swift_init/swift-container-replicator.conf'
+    }
+
+    file { '/etc/init/swift-container-updater.conf':
+      ensure  => present,
+      require => Package['swift-container'],
+      source  => 'puppet:///modules/swift/swift_init/swift-container-updater.conf'
+    }
+
+    file { '/etc/init/swift-container-auditor.conf':
+      ensure  => present,
+      require => Package['swift-container'],
+      source  => 'puppet:///modules/swift/swift_init/swift-container-auditor.conf'
+    }
+
+    service { 'swift-container':
+      ensure    => running,
+      enable    => true,
+      require   => File['/etc/init/swift-container.conf'],
+      subscribe => [ File['/etc/swift/container-server/1.conf'],
+                     File['/etc/swift/container-server/2.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-container-replicator':
+      ensure    => running,
+      enable    => true,
+      require   => File['/etc/init/swift-container-replicator.conf'],
+      subscribe => [ File['/etc/swift/container-server/1.conf'],
+                     File['/etc/swift/container-server/2.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-container-updater':
+      ensure    => running,
+      enable    => true,
+      require   => File['/etc/init/swift-container-updater.conf'],
+      subscribe => [ File['/etc/swift/container-server/1.conf'],
+                     File['/etc/swift/container-server/2.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
+    service { 'swift-container-auditor':
+      ensure    => running,
+      enable    => true,
+      require   => File['/etc/init/swift-container-auditor.conf'],
+      subscribe => [ File['/etc/swift/container-server/1.conf'],
+                     File['/etc/swift/container-server/2.conf'],
+                     File['/etc/swift/swift.conf']],
+    }
+
   }
 
   swift::ringcopy { ['container']: }
