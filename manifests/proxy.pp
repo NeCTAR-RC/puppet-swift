a6880da09e70056b3cccec79abd2681f65c77f54
Jake Yip <jake.yip@unimelb.edu.au>
2015-07-13 14:57:40 +1000

    Exclude installing python-keystonemiddleware
    Do not install python-keystonemiddleware if node is icehouse. Package
    exist only in versions Juno and after.
    
    Change-Id: I848e50c8a403257779ddba0d15a483be7b78ac8b

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -17,8 +17,13 @@ class swift::proxy($listen='0.0.0.0',
 
   $total_procs = 1 + $workers
 
-  package { 'swift-proxy':
-    ensure  => present,
+  package { ['swift-proxy', 'swift-plugin-s3']:
+    ensure => present,
+  }
+
+  case $openstack_version {
+    'icehouse': {}
+    default:    { package { 'python-keystonemiddleware': ensure => present } } 
   }
 
   if $swift::enable_ceilometer {
@@ -33,9 +38,6 @@ class swift::proxy($listen='0.0.0.0',
     }
   }
 
-  package { ['python-keystonemiddleware', 'swift-plugin-s3']:
-    ensure => installed,
-  }
 
   file { '/etc/swift/proxy-server.conf':
     ensure  => file,
