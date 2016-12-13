bff358324232ba1b9b15cfd7ab93c65d4314c242
Jake Yip <jake.yip@unimelb.edu.au>
2016-12-14 10:54:58 +1100

    Remove hyphens in class names
    And move nagios checks to nectar module
    
    Change-Id: Ida6b678cfd10712ee9acbaa4242751d7cfd04705

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -123,26 +123,7 @@ class swift::proxy($listen='0.0.0.0',
 }
 
 class swift::proxy::nagios-checks {
-  # These are checks that can be run by the nagios server.
-  nagios::command {
-    'check_swift_ssl':
-      check_command => '/usr/lib/nagios/plugins/check_http --ssl -u /healthcheck -p \'$ARG1$\' -H \'$HOSTADDRESS$\' -I \'$HOSTADDRESS$\'';
-    'check_swift':
-      check_command => '/usr/lib/nagios/plugins/check_http -u /healthcheck -p \'$ARG1$\' -H \'$HOSTADDRESS$\' -I \'$HOSTADDRESS$\'';
-    'check_swift_internal':
-      check_command => '/usr/lib/nagios/plugins/check_http -p \'$ARG1$\' -e 400 -H \'$HOSTADDRESS$\' -I \'$HOSTADDRESS$\'';
-    'check_swift_internal_ip':
-      check_command => '/usr/lib/nagios/plugins/check_http -p \'$ARG1$\' -e 400,405 -H \'$HOSTADDRESS$\' -I \'$ARG2$\'';
-    'check_swift_operations':
-      check_command => '/usr/local/lib/nagios/plugins/check_swift -A \'$ARG1$\' -U \'$ARG2$\' -P \'$ARG3$\' -T \'$ARG4$\' -R \'$ARG5$\' -c nagios';
-  }
+    include ::nectar::nagios::swift_proxy
 
-  file { '/usr/local/lib/nagios/plugins/check_swift':
-    ensure => present,
-    owner  => root,
-    group  => root,
-    mode   => '0775',
-    source => 'puppet:///modules/swift/check_swift',
-  }
-
+    notify {'class swift::proxy::nagios-checks is deprecated. Please use swift::proxy::nagios_checks': }
 }
