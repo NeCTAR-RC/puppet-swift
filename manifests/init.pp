21bd9f62ea1b6e88a1e7813b4974fa5cffb2e87b
Shahaan Ayyub <shahaan@gmail.com>
2017-06-27 11:21:40 +1000

    Adding check_replication_time nagios plugin.
    Change-Id: Ifd41bee77a1a3c32465220056121db66fe87d0eb

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,18 @@ class swift($swift_hash,
 
   $openstack_version = hiera('openstack_version')
 
+  # Brief Description: 
+  # These values define threshold limits (in seconds)
+  # for replicating swift accounts, containers and 
+  # objects. Warning and critical nagios alerts are 
+  # triggerred above these values
+  # Current Values:
+  # WARNING: 3 hours
+  # CRITICAL: 8 Hours
+
+  $nagios_warning_threshold = 10800
+  $nagios_critical_threshold = 28800
+
   package { 'swift':
     ensure => installed,
   }
