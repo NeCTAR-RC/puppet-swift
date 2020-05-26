e350d15f0e6869442729fd36eeaee990a6051f7c
Rocky <shi.yan@unimelb.edu.au>
2020-05-26 22:07:30 +1000

    Add rsync_bwlimit for object server
    Change-Id: I752dc30755febcfa94cb266c2faf3b7aedc35ca9

--- manifests/storage/all.pp
+++ manifests/storage/all.pp
@@ -151,6 +151,7 @@ class swift::storage::all(
   $object_server_workers          = $::os_workers,
   $object_server_mb_per_sync      = 512,
   $rsync_timeout                  = 3600,
+  $rsync_bwlimit                  = 0,
   $splice                         = false,
   $max_connections                = 25,
 ) {
@@ -227,5 +228,6 @@ from 6002 to 6202 and will be changed in a later release')
     splice                    => $splice,
     object_server_mb_per_sync => $object_server_mb_per_sync,
     rsync_timeout             => $rsync_timeout,
+    rsync_bwlimit             => $rsync_bwlimit,
   }
 }
